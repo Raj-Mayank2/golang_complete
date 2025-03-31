38d614483cfa509ff250bae95af72fe593105feb
Mayank Raj <153426642+Raj-Mayank2@users.noreply.github.com>
2025-03-31 12:42:54 +0530

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main(){
+    var a int=376
+    fmt.Println(a)
+    var as="this is true"
+    fmt.Println(as)
+    var ass =true
+    fmt.Println(ass)
+    f:="apple"
+    fmt.Println(f)
+}
